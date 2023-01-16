e4cd4d46e3aa3cbcbc821eb752c5ea9237d5f687
rushii <33725716+rushiiMachine@users.noreply.github.com>
2023-01-16 13:44:17 -0800

    now

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# shellcheck disable=2164
+# shellcheck disable=SC2164,SC2046
 # SC2164 cd fail
+# SC2046 word splitting (intentional with mv below)
 
+# move dist folder contents to build root
 cd build
-mv "$(jq -r '.distFolder' < aliucord.json)/*" .
+mv $(echo $(jq -r '.distFolder' < aliucord.json)/*) .
 
 # Delete any symlinks for safety
 find . -type l -delete
