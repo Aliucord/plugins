38010046ca94fea461337b7c95d25b53c1caed69
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2026-05-21 06:51:43 +0700

    feat: RazerTexz

--- build-logic/src/main/kotlin/com/aliucord/gradle/repo/Authors.kt
+++ build-logic/src/main/kotlin/com/aliucord/gradle/repo/Authors.kt
@@ -30,9 +30,13 @@ import com.aliucord.gradle.AliucordExtension
  */
 @Suppress("SpellCheckingInspection", "unused", "RedundantSuppression")
 object Authors {
-    val Aliucord = Author(name = "Aliucord")
-    val Ushie = Author("Ushie", 399862294143696897L, true)
+    val Aliucord = Author("Aliucord")
+    val Ushie = Author("Ushie", 399862294143696897L)
     val reisxd = Author("reisxd", 1385511666266935318L)
+    val Grzesiek11 = Author("Grzesiek11", 368475654662127616L)
+    val Alyxia = Author("Alyxia", 465702500146610176L)
+    val oSumAtrIX = Author("oSumAtrIX", 737323631117598811L)
+    val RazerTexz = Author("RazerTexz", 633565155501801472L)
 }
 
 /**
