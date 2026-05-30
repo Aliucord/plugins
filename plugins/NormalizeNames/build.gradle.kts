d37e9adc140d9e9bbcc48bf438f9a00ac8dbde36
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2026-05-30 00:35:44 -0700

    feat(plugins): add NormalizeNames

---
+++ plugins/NormalizeNames/build.gradle.kts
@@ -0,0 +1,38 @@
+import com.aliucord.gradle.repo.Authors
+import com.aliucord.gradle.repo.author
+
+plugins {
+    id("com.aliucord.plugins-repo")
+}
+
+version = "1.1.4"
+description = "Display nick/user names with some fancy chars reverted to normal."
+
+aliucord {
+    author(Authors.rushii)
+
+    changelog.set(
+        """
+			# 1.1.5
+			* Normalize user global names
+
+			# 1.1.4
+			* Discord 115104
+
+			# 1.1.3
+			* Require restart
+
+			# 1.1.2
+			* Fix changelog
+
+			# 1.1.1
+			* Update to new Discord
+
+			# 1.1.0
+			* Optionally remove diacritics
+
+			# 1.0.0
+			* Released
+		""".trimIndent(),
+    )
+}
---
+++ plugins/NormalizeNames/src/main/kotlin/dev/rushii/plugins/NormalizeNames.kt
@@ -0,0 +1,101 @@
+package dev.rushii.plugins
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.icu.text.Normalizer2
+import android.os.Bundle
+import android.view.View
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.after
+import com.aliucord.settings.delegate
+import com.aliucord.utils.ReflectUtils
+import com.aliucord.widgets.BottomSheet
+import com.discord.api.guildmember.GuildMember
+import com.discord.api.user.User
+import com.discord.views.CheckedSetting
+
+@Suppress("unused")
+@AliucordPlugin(requiresRestart = true)
+class NormalizeNames : Plugin() {
+    private val accentRegex = Regex("\\p{InCombiningDiacriticalMarks}+")
+
+    var normalizeDiacritics: Boolean by settings.delegate(false)
+    private lateinit var normalizer: Normalizer2
+
+    init {
+        settingsTab = SettingsTab(
+            NormalizeNamesSettings::class.java,
+            SettingsTab.Type.BOTTOM_SHEET,
+        ).withArgs(this)
+    }
+
+    private fun normalizeString(str: String): String {
+        var normalized = normalizer.normalize(str)
+        if (normalizeDiacritics)
+            normalized = normalized.replace(accentRegex, "")
+        return normalized
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun start(ctx: Context) {
+        normalizer = if (normalizeDiacritics) {
+            Normalizer2.getNFDInstance()!!
+        } else {
+            Normalizer2.getNFKCInstance()!!
+        }
+
+        patcher.after<User>("getUsername") {
+            if (it.result != null)
+                it.result = normalizeString(it.result as String)
+        }
+
+        patcher.after<GuildMember>("h") {
+            if (it.result != null)
+                it.result = normalizeString(it.result as String)
+        }
+
+        // Normalize user global names when they're added to a private cache by Aliucore core
+        val originalGlobalNames =
+            ReflectUtils.getField(Class.forName("com.aliucord.coreplugins.rn.PatchesKt"), null, "globalNames")
+        val globalNamesInterceptor = object : HashMap<Long, String>() {
+            override fun put(key: Long, value: String): String? {
+                return super.put(key, normalizeString(value))
+            }
+        }
+        ReflectUtils.setFinalField(
+            Class.forName("com.aliucord.coreplugins.rn.PatchesKt"),
+            null,
+            "globalNames",
+            globalNamesInterceptor,
+        )
+        globalNamesInterceptor.putAll(originalGlobalNames as Map<Long, String>)
+    }
+
+    override fun stop(context: Context) {
+        patcher.unpatchAll()
+    }
+}
+
+@SuppressLint("SetTextI18n")
+class NormalizeNamesSettings(private val plugin: NormalizeNames) : BottomSheet() {
+    override fun onViewCreated(view: View, bundle: Bundle?) {
+        super.onViewCreated(view, bundle)
+        val ctx = view.context
+
+        addView(
+            Utils.createCheckedSetting(
+                ctx, CheckedSetting.ViewType.SWITCH,
+                "Diacritics",
+                "Remove diacritics/accents on characters.",
+            ).apply {
+                isChecked = plugin.normalizeDiacritics
+                setOnCheckedListener {
+                    plugin.normalizeDiacritics = it
+                    Utils.promptRestart()
+                }
+            },
+        )
+    }
+}
