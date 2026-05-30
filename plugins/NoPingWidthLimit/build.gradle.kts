54562692b53ee6f844a388f32d1d43b9b8067141
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2026-05-29 22:51:12 -0700

    feat(plugins): add NoPingWidthLimit

---
+++ plugins/NoPingWidthLimit/build.gradle.kts
@@ -0,0 +1,33 @@
+import com.aliucord.gradle.repo.Authors
+import com.aliucord.gradle.repo.author
+
+plugins {
+    id("com.aliucord.plugins-repo")
+}
+
+version = "1.1.3"
+description = "Increase the max width of the ping count label."
+
+aliucord {
+    author(Authors.rushii)
+
+	changelog.set(
+		"""
+		# 1.1.3
+		* Fix changelog
+
+		# 1.1.2
+		* Force update pls
+
+		# 1.1.1
+		* Accidentally left in debug code
+
+		# 1.1.0
+		* Support text channel mention counter
+		* Support channel mentions
+
+		# 1.0.0
+		* Released
+		""".trimIndent()
+	)
+}
---
+++ plugins/NoPingWidthLimit/src/main/kotlin/dev/rushii/plugins/NoPingWidthLimit.kt
@@ -0,0 +1,59 @@
+package dev.rushii.plugins
+
+import android.content.Context
+import android.widget.TextView
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.*
+import com.aliucord.utils.DimenUtils
+import com.discord.databinding.WidgetHomeBinding
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter
+import com.discord.widgets.channels.list.items.ChannelListItem
+import com.discord.widgets.channels.list.items.ChannelListItemTextChannel
+import com.discord.widgets.home.*
+
+@Suppress("unused")
+@AliucordPlugin
+class NoPingWidthLimit : Plugin() {
+	private val channelMentionsId = Utils.getResId("channels_item_channel_mentions", "id")
+
+	override fun start(ctx: Context) {
+		patcher.after<WidgetHomeHeaderManager>(
+			"configure",
+			WidgetHome::class.java,
+			WidgetHomeModel::class.java,
+			WidgetHomeBinding::class.java
+		) {
+			val homeModel = (it.args[1] as WidgetHomeModel)
+			if (homeModel.unreadCount < 100) return@after // comment out when testing
+
+			val widgetHome = (it.args[0] as WidgetHome)
+			val mentionsView = widgetHome.unreadCountView
+
+			// testing
+			//			mentionsView.text = "69420"
+
+			mentionsView.layoutParams.width = DimenUtils.dpToPx(mentionsView.text.length * 7)
+		}
+
+		patcher.after<WidgetChannelsListAdapter.ItemChannelText>(
+			"onConfigure",
+			Integer.TYPE,
+			ChannelListItem::class.java
+		) {
+			val item = it.args[1] as ChannelListItemTextChannel
+			if (item.mentionCount < 100) return@after // comment out when testing
+			val mentionsView = this.itemView.findViewById<TextView>(channelMentionsId)
+
+			//			mentionsView.text = "69420" // testing
+
+			val length = mentionsView.text.length
+			mentionsView.layoutParams.width = DimenUtils.dpToPx(length * 9)
+		}
+	}
+
+	override fun stop(context: Context) {
+		patcher.unpatchAll()
+	}
+}
