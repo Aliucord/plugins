160be97eabfda867742e2f2b1b69214b266f13cd
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2026-05-30 00:39:27 -0700

    feat(plugins): add BetterTm

---
+++ plugins/BetterTm/build.gradle.kts
@@ -0,0 +1,32 @@
+import com.aliucord.gradle.repo.Authors
+import com.aliucord.gradle.repo.author
+
+plugins {
+    id("com.aliucord.plugins-repo")
+}
+
+version = "1.2.2"
+description = "Replaces the TM, R, and C emojis with ones that are more visible."
+
+aliucord {
+    author(Authors.rushii)
+
+    changelog.set(
+        """
+			# 1.2.2
+			* Fix target resource IDs
+
+			# 1.2.1
+			* Fix changelog
+
+			# 1.2.0
+			* Add original emojis as an option
+
+			# 1.1.0
+			* Add Registered Trademark & Copyright emoji
+
+			# 1.0.0
+			* Released
+		""".trimIndent(),
+    )
+}
---
+++ plugins/BetterTm/src/main/kotlin/dev/rushii/plugins/BetterTm.kt
@@ -0,0 +1,94 @@
+package dev.rushii.plugins
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import com.aliucord.Utils
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.api.SettingsAPI
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.PreHook
+import com.aliucord.widgets.BottomSheet
+import com.discord.utilities.images.MGImages
+import com.discord.views.CheckedSetting
+
+// FIXME: use resources rather than remote url
+const val BASE_URL = "https://raw.githubusercontent.com/rushiiMachine/aliucord-plugins/master/BetterTm/"
+
+const val TM_EMOJI_URI = "res:///2131823865"
+const val TM_URL = BASE_URL + "tm.png"
+const val TM_ORIGINAL_URL = BASE_URL + "tm_original.png"
+
+const val R_EMOJI_URI = "res:///2131824088"
+const val R_URL = BASE_URL + "r.png"
+const val R_ORIGINAL_URL = BASE_URL + "r_original.png"
+
+const val C_EMOJI_URI = "res:///2131824087"
+const val C_URL = BASE_URL + "c.png"
+const val C_ORIGINAL_URL = BASE_URL + "c_original.png"
+
+const val USE_ORIGINAL_KEY = "useOriginal"
+
+@Suppress("unused")
+@AliucordPlugin(requiresRestart = true)
+class BetterTm : Plugin() {
+    private infix fun String.orOriginal(originalUrl: String) =
+        if (settings.getBool(USE_ORIGINAL_KEY, false)) originalUrl else this
+
+    init {
+        settingsTab = SettingsTab(
+            BetterTmSettings::class.java,
+            SettingsTab.Type.BOTTOM_SHEET,
+        ).withArgs(settings)
+    }
+
+    override fun start(ctx: Context) {
+        patcher.patch(
+            MGImages::class.java.getMethod(
+                "getImageRequest",
+                String::class.java,
+                Integer.TYPE,
+                Integer.TYPE,
+                java.lang.Boolean.TYPE,
+            ),
+            PreHook {
+                // logger.info(it.args[0] as String)
+
+                it.args[0] = when (it.args[0]) {
+                    TM_EMOJI_URI -> TM_URL orOriginal TM_ORIGINAL_URL
+                    R_EMOJI_URI -> R_URL orOriginal R_ORIGINAL_URL
+                    C_EMOJI_URI -> C_URL orOriginal C_ORIGINAL_URL
+                    else -> return@PreHook
+                }
+
+                it.args[1] = 100
+                it.args[2] = 100
+            },
+        )
+    }
+
+    override fun stop(context: Context) {
+        patcher.unpatchAll()
+    }
+}
+
+class BetterTmSettings(private val settings: SettingsAPI) : BottomSheet() {
+    override fun onViewCreated(view: View, bundle: Bundle?) {
+        super.onViewCreated(view, bundle)
+        val ctx = view.context
+
+        addView(
+            Utils.createCheckedSetting(
+                ctx, CheckedSetting.ViewType.SWITCH,
+                "Originals",
+                "Use original emojis made white instead of replacements.",
+            ).apply {
+                isChecked = settings.getBool(USE_ORIGINAL_KEY, false)
+                setOnCheckedListener {
+                    settings.setBool(USE_ORIGINAL_KEY, it)
+                    Utils.promptRestart()
+                }
+            },
+        )
+    }
+}
